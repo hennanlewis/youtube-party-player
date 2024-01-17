e2fc9c4e8b492b3011305b1d9c5beae1dfc9064e
hennanlewis <39320310+hennanlewis@users.noreply.github.com>
2024-01-16 21:39:05 -0300

    feat: redirect to party url

--- yt-party-player/index.js
+++ yt-party-player/index.js
@@ -5,8 +5,14 @@ const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/:id", (req, res) => {
+    res.sendFile(__dirname + "/public/party.html")
 })
 
 server.listen(3000, () => {
